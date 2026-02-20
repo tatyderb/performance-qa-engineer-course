da49dbe29ff284d5f33bf87eb68d05750ccaca93
tatyderb <tatyderb@gmail.com>
2026-02-20 22:09:47 +0300

    14.3.14 Эксперименты с grpc server - хардкодим 9007.

--- services/payments/server/grpc.py
+++ services/payments/server/grpc.py
@@ -17,11 +17,30 @@ from libs.logger import get_logger
 from libs.grpc.server.interceptors.logger_interceptor import GRPCLoggerInterceptor
 from libs.grpc.server.interceptors.exception_interceptor import GRPCExceptionInterceptor
 
+# async def serve():
+#     logger = get_logger("MOCK_PAYMENT_SERVICE_GRPC_SERVER")
+#     # Создаём gRPC-сервер с пулом потоков на 10 воркеров
+#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+#
+#     # Регистрируем наш сервис на сервере
+#     payments_service_pb2_grpc.add_PaymentsServiceServicer_to_server(PaymentsMockService(), server)
+#     # greeting_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+#
+#     # Назначаем порт, на котором будет слушать сервер
+#     server.add_insecure_port('[::]:9007')  # [::] — для IPv4/IPv6
+#
+#     print("Запуск сервера на порту 9007...")
+#     await server.start()  # Запуск сервера
+#     await server.wait_for_termination()  # Ожидание завершения (бесконечно)
+
+
 async def serve():
     logger = get_logger("MOCK_PAYMENT_SERVICE_GRPC_SERVER")
-    server = build_grpc_server(settings.mock_grpc_server, logger)
+    server = build_grpc_server(settings.payments_grpc_server, logger)
 
     payments_service_pb2_grpc.add_PaymentsServiceServicer_to_server(PaymentsMockService(), server)
+    # Назначаем порт, на котором будет слушать сервер
+    server.add_insecure_port('[::]:9007')  # [::] — для IPv4/IPv6
 
     reflection.enable_server_reflection(
         (
